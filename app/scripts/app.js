c3f61ea2cf5f966533604736a2711d274045e6a7
Ernesto Araiza <yo@ernestoaraiza.com>
2016-01-20 07:57:47 -0500

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,70 @@
+var app = angular.module('confusionApp',[]);
+		app.controller('menuController', function(){
+
+			this.tab = 1;
+			this.filtText = '';
+
+			var dishes=[
+					{
+						name:'Uthapizza',
+						image: 'images/uthapizza.png',
+						category: 'mains',
+						label:'Hot',
+						price:'4.99',
+						description:'Short loin jerky venison, shank ball tip sausage tail meatball chicken salami meatloaf. Hamburger leberkas turkey venison ground round pork belly pastrami pork picanha pork loin chicken biltong.',
+						comment:''
+					},
+					{
+						name:'Zucchipakoda',
+                  image: 'images/zucchipakoda.png',
+                  category: 'appetizer',
+                  label:'',
+                  price:'1.99',
+                  description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+                  comment: ''
+					},
+					{
+						name:'Vadonut',
+                  image: 'images/vadonut.png',
+                  category: 'appetizer',
+                  label:'New',
+                  price:'1.99',
+                  description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
+                  comment: ''
+					},
+					{
+                  name:'ElaiCheese Cake',
+                  image: 'images/elaicheesecake.png',
+                  category: 'dessert',
+                  label:'',
+                  price:'2.99',
+                  description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+                  comment: ''
+               }
+					];
+
+			this.dishes = dishes;
+
+			this.select = function(setTab){
+				this.tab = setTab;
+
+				switch(setTab){
+					case 2:
+						this.filtText = "appetizer";
+						break;
+					case 3:
+						this.filtText = "mains";
+						break;
+					case 4:
+						this.filtText = "dessert";
+						break;
+					default:
+						this.filtText = "";
+				}
+
+			}
+
+			this.isSelected = function(val){
+				return (this.tab === val);
+			}
+		});
