b0d7e1cba54a2bb0de7e281b9cff7b6131c0eeb1
Ernesto Araiza <yo@ernestoaraiza.com>
2016-01-24 21:29:56 -0500

    before exercise on forms and validation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('confusionApp',[])
-		.controller('menuController', function(){
+		.controller('MenuController', ['$scope',function($scope){
 
-			this.tab = 1;
-			this.filtText = '';
+			$scope.tab = 1;
+			$scope.filtText = '';
+			$scope.showDetails = false;
 
-			var dishes=[
+			$scope.dishes=[
 					{
 						name:'Uthapizza',
 						image: 'images/uthapizza.png',
@@ -45,28 +46,30 @@ angular.module('confusionApp',[])
                }
 					];
 
-			this.dishes = dishes;
-
-			this.select = function(setTab){
-				this.tab = setTab;
+			$scope.select = function(setTab){
+				$scope.tab = setTab;
 
 				switch(setTab){
 					case 2:
-						this.filtText = "appetizer";
+						$scope.filtText = "appetizer";
 						break;
 					case 3:
-						this.filtText = "mains";
+						$scope.filtText = "mains";
 						break;
 					case 4:
-						this.filtText = "dessert";
+						$scope.filtText = "dessert";
 						break;
 					default:
-						this.filtText = "";
+						$scope.filtText = "";
 				}
 
 			};
 
-			this.isSelected = function(val){
-				return (this.tab === val);
+			$scope.isSelected = function(val){
+				return ($scope.tab === val);
 			};
-		});
+
+			$scope.toggleDetails = function(){
+				$scope.showDetails = !$scope.showDetails;
+			};
+		}]);
