d8bc3b4431225d31d0db7a9945ec018f3bc7f62f
Ernesto Araiza <yo@ernestoaraiza.com>
2016-01-25 02:16:53 -0500

    hiding the preview when invalid

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -160,25 +160,22 @@ angular.module('confusionApp',[])
 	.controller('DishCommentController', ['$scope', function($scope) {
           
           //Step 1: Create a JavaScript object to hold the comment from the form
-          $scope.comment = {firstName:"", rating:5, comentario:"", date:""};
-
-          /*$scope.ratings = [{value:1, label:"1"},
-												{value:2, label:"2"},
-												{value:3, label:"3"},
-												{value:4, label:"4"},
-												{value:5, label:"5"}];*/
+          $scope.comment = {rating:5, comment:"", author:"", date:""};
 
           $scope.submitComment = function () {
               
               //Step 2: This is how you record the date
-              $scope.date = new Date().toISOString();
+              $scope.comment.date = new Date().toISOString();
               
               // Step 3: Push your comment into the dish's comment array
               $scope.dish.comments.push($scope.comment);
               
               //Step 4: reset your form to pristine
+            	$scope.commentForm.$setPristine();
               
               //Step 5: reset your JavaScript object that holds your comment
+              $scope.comment = {rating:5, comment:"", author:"", date:""};
+            
           }
       }])
 
