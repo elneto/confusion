4be2900244f689a02296132f2c8626b02cfa2847
Ernesto Araiza <yo@ernestoaraiza.com>
2016-01-30 16:13:18 -0500

    posting comments to the server, updating via put

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -7,10 +7,18 @@ angular.module('confusionApp')
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
-            $scope.showMenu = true;
+            $scope.showMenu = false;
             $scope.message = "Loading...";
 
-            $scope.dishes = menuFactory.getDishes().query();
+            menuFactory.getDishes().query(
+                function(response){
+                    $scope.dishes = response;
+                    $scope.showMenu = true;
+                },
+                function(response){
+                    $scope.message = "Error: " + response.status + " " + response.statusText;
+                }
+            );
                         
             $scope.select = function(setTab) {
                 $scope.tab = setTab;
@@ -38,14 +46,25 @@ angular.module('confusionApp')
             };
         }])
 
-        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
+        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', 
+            function($scope, $stateParams, menuFactory) {
 
-            $scope.showDish = true;
-            $scope.message = "Loading...";
+                $scope.dish = {};
+                $scope.showDish = false;
+                $scope.message = "Loading...";
 
-            $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)});
+                $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)})
+                    .$promise.then(
+                        function(response){
+                            $scope.dish = response;
+                            $scope.showDish = true;
+                        },
+                        function(response){
+                            $scope.message = "Error: " + response.status + " " + response.statusText;
+                        }
+                    );
             
-        }])
+            }])
 
         .controller('ContactController', ['$scope', function($scope) {
 
@@ -78,7 +97,8 @@ angular.module('confusionApp')
             };
         }])
 
-        .controller('DishCommentController', ['$scope', function($scope) {
+        .controller('DishCommentController', ['$scope', 'menuFactory', 
+            function($scope, menuFactory) {
             
             $scope.mycomment = {rating:5, comment:"", author:"", date:""};
             
@@ -88,6 +108,8 @@ angular.module('confusionApp')
                 console.log($scope.mycomment);
                 
                 $scope.dish.comments.push($scope.mycomment);
+
+                menuFactory.getDishes().update({id:$scope.dish.id}, $scope.dish);
                 
                 $scope.commentForm.$setPristine();
                 
@@ -95,14 +117,22 @@ angular.module('confusionApp')
             };
         }])
 
-        // implement the IndexController and About Controller here
         .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', 
                                 function($scope, menuFactory, corporateFactory) {
 
-            $scope.showDish = true;
+            $scope.showDish = false;
             $scope.message = "Loading...";
 
-            $scope.dish = menuFactory.getDishes().get({id:0});
+            $scope.dish = menuFactory.getDishes().get({id:0})
+                .$promise.then(
+                    function(response){
+                        $scope.dish = response;
+                        $scope.showDish = true;
+                    },
+                    function(response){
+                        $scope.message = "Error: " + response.status + " " + response.statusText;
+                    }
+                );
 
             $scope.promotion = menuFactory.getPromotion(0);
             $scope.leader = corporateFactory.getLeader(3);
