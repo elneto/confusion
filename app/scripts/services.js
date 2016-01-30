d2f961bba05eab6edfcdb7a4fda72ebd7a26dbdd
Ernesto Araiza <yo@ernestoaraiza.com>
2016-01-30 00:39:32 -0500

    finished assignment 3

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -199,6 +199,10 @@ angular.module('confusionApp')
     
                 // implement a function named getPromotion
                 // that returns a selected promotion.
+                this.getPromotion = function (index) {
+                    
+                    return promotions[index];
+                };
     
                         
         })
@@ -224,7 +228,7 @@ angular.module('confusionApp')
                 },
                 {
                     name: "Agumbe Tang",
-                          image: 'images/alberto.png',
+                    image: 'images/alberto.png',
                     designation: "Chief Taste Officer",
                     abbr: "CTO",
                     description: "Blessed with the most discerning gustatory sense, Agumbe, our CFO, personally ensures that every dish that we serve meets his exacting tastes. Our chefs dread the tongue lashing that ensues if their dish does not meet his exacting standards. He lives by his motto, You click only if you survive my lick."
@@ -240,9 +244,16 @@ angular.module('confusionApp')
             ];
      
             // Implement two functions, one named getLeaders,
-            // the other named getLeader(index)
-            // Remember this is a factory not a service
+            corpfac.getLeaders = function(){
+                    return leadership;    
+                };
     
+            // the other named getLeader(index)
+            corpfac.getLeader = function (index) {            
+                    return leadership[index];
+                };
+            // Remember this is a factory not a service
+            return corpfac;
     
         })
 
